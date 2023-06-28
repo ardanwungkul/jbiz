6185f03f3bd1a25b695bd7f1c132509b019fcff6
ardanwungkul <ardanwungkul14@gmail.com>
2023-06-28 09:30:42 +0700

    a

--- app/Http/Controllers/PelangganController.php
+++ app/Http/Controllers/PelangganController.php
@@ -112,13 +112,15 @@ class PelangganController extends Controller
 
         );
 
+        $charactersToRemove = ['0', '62'];
+        $string = ltrim($request->no_hp, implode('', $charactersToRemove));
 
         // dd($request);
 
         $pelanggan = new Pelanggan();
         $pelanggan->nama_pelanggan = $request->nama_pelanggan;
         $pelanggan->alamat = $request->alamat;
-        $pelanggan->no_hp = $request->no_hp;
+        $pelanggan->no_hp = '62' . $string;
         $pelanggan->keterangan_pelanggan = $request->keterangan_pelanggan;
         $pelanggan->link_history = $request->link_history;
         $pelanggan->user_id = $request->user_id;
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,52 +7,66 @@
 
     <div class=" mt-5">
         <div class="flex justify-center gap-10">
-            <div class="w-56 rounded-2xl dashItem flex items-center py-5 px-4">
-                <div
-                    class="w-10 h-10 bg-white backdrop-opacity-10 backdrop-blur-xl rounded-full flex justify-center items-center">
-                    <i class="fa-solid fa-globe"></i>
-                </div>
-                <div class="pl-4">
+            <a href="{{ route('domain.index') }}" class="group hover:scale-110 transition ease-out duration-1000">
+                <div class="w-60 rounded-2xl dashItem flex items-center py-5 px-4 border border-gray-400">
                     <div>
-                        <h2 class="text-white">
-                            {{ $totalDomain }}
-                        </h2>
-                        <h2 class="text-white">
-                            Total Domain
-                        </h2>
+                        <div
+                            class="w-12 h-12 group-hover:animate-bounce duration-1000 motion-reduce:animate-bounce bg-gray-800 border border-gray-400 rounded-full flex justify-center items-center">
+                            <i class="fa-solid text-white fa-globe"></i>
+                        </div>
+                    </div>
+                    <div class="pl-4 w-full">
+                        <div>
+                            <h2 class="text-white text-2xl text-center font-extrabold">
+                                {{ $totalDomain }}
+                            </h2>
+                            <h2 class="text-gray-300 text-sm text-center">
+                                Total Domain
+                            </h2>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="w-56 rounded-2xl dashItem flex items-center py-5 px-4">
-                <div class="w-10 h-10 bg-white rounded-full flex justify-center items-center">
-                    <i class="fa-solid fa-person fa-xl"></i>
-                </div>
-                <div class="pl-4">
+            </a>
+            <a href="{{ route('pelanggan.index') }}" class="group hover:scale-110 transition ease-out duration-1000">
+                <div class="w-60 rounded-2xl dashItem flex items-center py-5 px-4 border border-gray-400">
                     <div>
-                        <h2 class="text-white">
-                            {{ $totalPelanggan }}
-                        </h2>
-                        <h2 class="text-white">
-                            Total Pelanggan
-                        </h2>
+                        <div
+                            class="w-12 h-12 group-hover:animate-bounce duration-1000 motion-reduce:animate-bounce bg-gray-800 border border-gray-400 rounded-full flex justify-center items-center">
+                            <i class="fa-solid text-white fa-person fa-xl"></i>
+                        </div>
+                    </div>
+                    <div class="pl-4 w-full">
+                        <div>
+                            <h2 class="text-white text-2xl text-center font-extrabold">
+                                {{ $totalPelanggan }}
+                            </h2>
+                            <h2 class="text-gray-300 text-sm text-center">
+                                Total Pelanggan
+                            </h2>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="w-56 rounded-2xl dashItem flex items-center py-5 px-4">
-                <div class="w-10 h-10 bg-white rounded-full flex justify-center items-center">
-                    <i class="fa-solid fa-user"></i>
-                </div>
-                <div class="pl-4">
+            </a>
+            <a href="{{ route('user.index') }}" class="group hover:scale-110 transition ease-out duration-1000">
+                <div class="w-60 rounded-2xl dashItem flex items-center py-5 px-4 border border-gray-400">
                     <div>
-                        <h2 class="text-white">
-                            {{ $totalUser }}
-                        </h2>
-                        <h2 class="text-white">
-                            Total User
-                        </h2>
+                        <div
+                            class="w-12 h-12 group-hover:animate-bounce duration-1000 motion-reduce:animate-bounce bg-gray-800 border border-gray-400 rounded-full flex justify-center items-center">
+                            <i class="fa-solid text-white fa-user"></i>
+                        </div>
+                    </div>
+                    <div class="pl-4 w-full">
+                        <div>
+                            <h2 class="text-white text-2xl text-center font-extrabold">
+                                {{ $totalUser }}
+                            </h2>
+                            <h2 class="text-gray-300 text-sm text-center">
+                                Total User
+                            </h2>
+                        </div>
                     </div>
                 </div>
-            </div>
+            </a>
         </div>
     </div>
 
